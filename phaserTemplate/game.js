dd983c226f887727b06a84eadc53f0782a12a3f9
marylynntom <marylynntom@gmail.com>
2018-03-15 17:29:25 -0700

    worked with template, new jobs

--- phaserTemplate/game.js
+++ phaserTemplate/game.js
@@ -1,3 +1,5 @@
+var map;
+
 // Create the main game state class
 const gameState = function () { };
 
@@ -8,21 +10,35 @@ gameState.prototype = {
   preload: function () {
 
     /*** Load assets here ***/
+    this.load.image('map', 'img/map.jpg');
   
   },
 
   // Phaser function to initialize variables and instantiate objects
   create: function () {
-
+      
+    // game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    // game.scale.setScreenSize(true);
+      
     // Add some text
     text = game.add.text(300, 280, '', { fontSize: '32px', fill: '#fff' });
+      
+    map = this.add.sprite(0, 0, 'map');
+      
+    this.input.mouse.capture = true;
   },
 
   // Phaser main game function to update the screen
   update: function () {
       
     // Show mouse coordinates for debugging and placing objects
-    text.setText("X: " + game.input.mousePointer.x + "  Y: " + game.input.mousePointer.y);
+    // text.setText("X: " + game.input.mousePointer.x + "  Y: " + game.input.mousePointer.y);
+      
+    // game.debug.text("Left Button: " + this.input.activePointer.leftButton.isDown, 300, 132);
+      
+    if (this.input.activePointer.leftButton.isDown) {
+        map.scale.setTo(1.5);
+    }
   }
 };
 
