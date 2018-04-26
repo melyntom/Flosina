872169dc52e1c0f65993552f3304f18feedd11b4
Emi <emiarriola@gmail.com>
2018-04-26 16:56:54 -0700

    Update forest.js

--- states/forest.js
+++ states/forest.js
@@ -1,6 +1,8 @@
 // The Forest in the Outskirts of Blurpia
 // Home to Emi's Character
 
+var lily;
+
 const forestState = {
 
     preload: function () {
@@ -8,12 +10,16 @@ const forestState = {
         game.load.image('backdrop', 'img/forestKingdom.png');
         game.load.image('map', 'img/main.png');
         
+        game.load.spritesheet('lily', 'img/Lily.png', 32, 32, 13);
     },
 
     create: function () {
         
         game.add.sprite(0, 0, 'backdrop');
         console.log("Forest!");
+        
+        lily = game.add.sprite(400, 400, 'mellie');
+        lily.scale.setTo(4, 4);
         
         // Add button to go to back to Map
         
@@ -30,6 +36,26 @@ const forestState = {
     },
 
     update: function () {
+        
+        const lilyFrames = [];
+        
+        for (let i = 1; i < 14; i++) {
+            lilyFrames.push(i);
+        }
+        
+        lily.animations.add('play', lilyFrames, 10, true);
+
+        // Add a function to the button to be called when the button is clicked
+        map.events.onInputDown.add(function () {
+            game.state.start('game');
+        }, this);
+
+    },
+
+    update: function () {
+
+        // Animate char: Mellie
+        lily.animations.play('play');
 
         
         
