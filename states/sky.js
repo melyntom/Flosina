621d11bf679a99f1a26d521a2adfeccda86e2d6e
marylynntom <marylynntom@gmail.com>
2018-04-26 17:45:23 -0700

    sky updates

--- states/sky.js
+++ states/sky.js
@@ -7,10 +7,10 @@ const skyState = {
 
     preload: function () {
 
-        game.load.image('backdrop', 'img/skyKingdom.jpg');
+        game.load.image('backdrop', 'img/skyKingdom.png');
         game.load.image('map', 'img/main.png');
         
-         game.load.spritesheet('angelw', 'img/charAngelWings.png', 32, 32, 13);
+         game.load.spritesheet('angelw', 'img/AngelWings.png', 32, 32, 13);
         
     },
 
@@ -18,31 +18,32 @@ const skyState = {
         
         game.world.setBounds(0, 0, 1042, 666);
         
+        // Set backdrop
         game.add.sprite(0, 0, 'backdrop');
         console.log("Sky!");
         
-        // Add button to go to back to Map
+        // Add AngelW character
+        angelw = game.add.sprite(100, 320, 'angelw');
         
+        // Add button to go to back to Map
         const map = game.add.sprite(50, 50, 'map');
         map.anchor.set(0.5);
         map.inputEnabled = true;
         map.input.useHandCursor = true;  // Change cursor style on mouseover
+        
+        // Add AngelW animations
+        const angelwFrames = [];
+        
+        for (let i = 1; i < 14; i++) {
+            angelwFrames.push(i);
+        }
+        
+        angelw.animations.add('play', angelwFrames, 10, true);
 
         // Add a function to the button to be called when the button is clicked
         map.events.onInputDown.add(function () {
             game.state.start('game');
         }, this);
-        
-        game.add.sprite(400, 400, 'angelw');
-        
-        // Add Angel WIngs animations
-        
-        const angelwFrames = [];
-        for (let i = 1; i < 14; i++) {
-            angelwFrames.push(i);
-        }   
-        
-        angelw.animations.add('play', angelwFrames, 10, true);
         
     },
             
