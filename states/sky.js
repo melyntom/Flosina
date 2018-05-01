ac4f6a8e775ef62a5fb812fe4488093dc6cd70d3
awslays <36682489+awslays@users.noreply.github.com>
2018-05-01 16:33:11 -0700

    Update sky.js

--- states/sky.js
+++ states/sky.js
@@ -23,7 +23,7 @@ const skyState = {
         console.log("Sky!");
         
         // Add AngelW character
-        angelw = game.add.sprite(255, 255, 'angelw');
+        angelw = game.add.sprite(275, 275, 'angelw');
         
         // Add button to go to back to Map
         const map = game.add.sprite(50, 50, 'map');
