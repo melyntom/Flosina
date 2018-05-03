f7cdb9281888471d85499ef9ee0c31f5e3d8a863
awslays <36682489+awslays@users.noreply.github.com>
2018-05-03 16:12:19 -0700

    Update sky.js

--- states/sky.js
+++ states/sky.js
@@ -29,7 +29,7 @@ const skyState = {
         console.log("Sky!");
         
         // Add AngelW character
-        angelw = game.add.sprite(350, 200, 'angelw');
+        angelw = game.add.sprite(350, 275, 'angelw');
         
         angelw.scale.setTo(0.7);
         
