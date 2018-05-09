86e7f245b8190bd4018a2dcc8b59f47a51356afd
Mary Lynn Tom <marylynntom@gmail.com>
2018-05-08 22:36:25 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -40,7 +40,7 @@ const menuState = {
     text2.setTextBounds(0, game.world.centerY + 260, 800, 100);
       
     // Add logo
-    logo = game.add.sprite(125, 0, 'logo');
+    logo = game.add.sprite(125, -28, 'logo');
       
     const logoFrames = [];
         
